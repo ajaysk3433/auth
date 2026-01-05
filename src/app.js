7bf42b79f5721cc69a5f4e398569a6d745451d0f
Ajay <ajaysk3433@gmail.com>
2026-01-05 13:44:56 +0530

    Add helmet and express-rate-limit for enhanced security and request limiting

--- src/app.js
+++ src/app.js
@@ -3,9 +3,18 @@ const cookieParser = require("cookie-parser");
 const authHandler = require("./auth/authentication");
 require("dotenv").config();
 const app = express();
+const helmet = require("helmet");
+const { rateLimit } = require("express-rate-limit");
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(helmet());
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 10, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+});
+app.use(limiter);
 
 app.use("/auth", authHandler);
 
